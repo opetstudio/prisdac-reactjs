49de273e19419d84129570f22def807f6f095354
Nofrets Poai <opetstudio@gmail.com>
2019-03-23 08:52:47 +0700

    ok

--- build.sh
+++
@@ -1,5 +0,0 @@
-git rm -rf .
-git checkout HEAD -- .gitignore .htaccess build.sh dbconnect.php
-cp -rf ~/workspace_electron/mypos/build/* .
-mv index.html react-index.html
-
--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-Class DbConnection{
-    function getdbconnect(){
-        $conn = mysqli_connect("localhost","root","password","adra_db") or die("Couldn't connect");
-
-        return $conn;
-    }
-}
-?>
